d42771d8ddb41471045e5a46d34db55899559f57
Ramón AP Quintana <atdiel.perez9709@gmail.com>
2022-05-31 01:22:38 -0500

    few changes within DAL/models/index.js 🛴

--- DAL/models/index.js
+++ DAL/models/index.js
@@ -1,6 +1,9 @@
 const models = {
   userModel: require("./userModel"),
   tabataModel: require("./tabataModel"),
+  routineModel: require("./routineModel"),
+  recordModel: require("./recordModel"),
+  challengeModel: require("./challengeModel"),
   favoriteWorkoutModel: require("./favoriteWorkoutModel"),
 };
 
