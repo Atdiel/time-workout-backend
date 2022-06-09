ce6a83cadc34e04e9bf93b7e85cd92347792a4ac
Ramón AP Quintana <atdiel.perez9709@gmail.com>
2022-06-09 07:32:14 -0500

    using a incoming webhook 🕵️‍♂️

--- BLL/api/app.js
+++ BLL/api/app.js
@@ -1,11 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const loggerStream = require("./tools/loggerWebhook");
+const morganBody = require("morgan-body");
+const swaggerUI = require("swagger-ui-express");
+const openAPIConfiguration = require("./docs/swagger");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+/**
+ * > Mandamos logs de errores a nuestro Webhook
+ *
+ */
+morganBody(app, {
+  noColors: true,
+  stream: loggerStream,
+  skip: function (req, res) {
+    return res.statusCode < 400;
+  },
+});
 
 /**
  * se usa el puerto definido en .env (default 3000)
@@ -15,9 +31,11 @@ const port = process.env.PORT || 3000;
 /**
  * route homepage
  */
-app.route("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to Time-Workout Backend");
 });
+
+app.use("/docs/v1", swaggerUI.serve, swaggerUI.setup(openAPIConfiguration));
 
 /**
  * usamos ./routes/index.js para manejar endpoints
