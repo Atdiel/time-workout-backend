729c2b833a554f29ed67895f42c6f2620b050c37
Atdiel <atdiel.perez9709@gmail.com>
2022-07-06 23:41:14 -0500

    🐣🐣BETA - modified the handleError again due to better future manage of the status code

--- BLL/api/handlers/handleError.js
+++ BLL/api/handlers/handleError.js
@@ -3,16 +3,14 @@ const loggerWebhook = require("../tools/loggerWebhook");
 /**
  * > manage the error responses to the client.
  * @param {JSON} res Object response provided for the server.
- * @param {Array} err Error Object
+ * @param {Array} err Error Object.
  */
 const handleHttpError = (res, err) => {
+  //TODO: create the handleLogger function.
   handleLogger();
+
   if (err[0]) {
-    if (err[1]) {
-      res.status(500);
-    } else {
-      res.status(400);
-    }
+    res.status(err[2]);
     res.send({ success: false, mssg: err[0] });
   } else {
     res.status(500).send({ success: false, mssg: "Something was wrong" });
