fdef57263700c4904451582d91e307843a0d5bae
Atdiel <atdiel.perez9709@gmail.com>
2022-07-07 17:24:41 -0500

    :octocat: fix: add a best Error response

--- BLL/api/handlers/handleValidator.js
+++ BLL/api/handlers/handleValidator.js
@@ -1,15 +1,22 @@
 const { validationResult } = require("express-validator");
 
+/**
+ * > Handle if exists a few errors that arrive
+ * > from the validators.
+ * @param {*} req
+ * @param {*} res
+ * @param {Callback} next
+ */
 const handleResults = (req, res, next) => {
   try {
     validationResult(req).throw();
     return next();
   } catch (err) {
-    const data = {
+    res.status(400).send({
       success: false,
+      mssg: "Verify your form request body",
       errors: err.array(),
-    };
-    res.status(403).send(data);
+    });
   }
 };
 
