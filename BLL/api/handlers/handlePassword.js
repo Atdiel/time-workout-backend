6dfaea3bb55b6319c4df122d6eb15d7c4fbf7bea
Atdiel <atdiel.perez9709@gmail.com>
2022-07-08 23:53:55 -0500

    fix: add use promise in Handle Password Class :octocat:

--- BLL/api/handlers/handlePassword.js
+++ BLL/api/handlers/handlePassword.js
@@ -2,22 +2,38 @@ const bcryptjs = require("bcryptjs");
 const saltOrRounds = 10;
 
 /**
- * Encriptar contraseña en texto plano
- * @param {*} plainPassword
- * @returns
+ * > Encrypt password.
+ * @param {String} plainPassword Password in text plain.
+ * @returns {Promise<String>} Password encrypted.
  */
 const hashPassword = async (plainPassword) => {
-  return await bcryptjs.hash(plainPassword, saltOrRounds);
+  return new Promise(async (resolve, reject) => {
+    try {
+      const hashResulting = await bcryptjs.hash(plainPassword, saltOrRounds);
+
+      resolve(hashResulting);
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
 
 /**
- * Comparar una contraseña plana y una encriptada.
- * @param {*} plainPassword
- * @param {*} hashedPassword
- * @returns
+ * > Compare a password in text plain against an encrypted.
+ * @param {String} plainPassword
+ * @param {String} hashedPassword
+ * @returns {Promise}
  */
-const comparePassword = async (plainPassword, hashedPassword) => {
-  return await bcryptjs.compare(plainPassword, hashedPassword);
+const comparePassword = (plainPassword, hashedPassword) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await bcryptjs.compare(plainPassword, hashedPassword);
+
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
 
 module.exports = { hashPassword, comparePassword };
