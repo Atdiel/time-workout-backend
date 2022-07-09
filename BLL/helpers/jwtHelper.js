cd35a1eb309c682bf494b1976f178de3ddad40b0
Atdiel <atdiel.perez9709@gmail.com>
2022-07-08 23:51:38 -0500

    fix: remove repeated code and improve handleJwt :octocat:

--- BLL/helpers/jwtHelper.js
+++ BLL/helpers/jwtHelper.js
@@ -1,20 +1,44 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
-/**
- * firmar token con datos del usuario y secret del backend @constant JWT_SECRET
- * @param {*} userObject usuario
- * @returns token: string
- */
-const signToken = async (userObject) => {
-  const token = jwt.sign(
-    {
-      _id: userObject.userId,
-      name: userObject.name,
-    },
-    JWT_SECRET
-  );
 
-  return token;
+/**
+ * > Sign token with user data and backend secret.
+ * @param {JSON} userObject user DTO Object in JSON format.
+ * @returns {Promise<String>} new token generated.
+ */
+const signToken = (userObject) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const token = jwt.sign(
+        {
+          _id: userObject.userId,
+          name: userObject.name,
+        },
+        JWT_SECRET
+      );
+
+      resolve(token);
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
 
-module.exports = { signToken };
+/**
+ * > Verify that the token has been signed on this server.
+ * @param {String} tokenJwt A Json Web Token given.
+ * @returns {Promise<JSON>} The decoded token
+ */
+const verifyToken = (tokenJwt) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const decodedToken = jwt.verify(tokenJwt, JWT_SECRET);
+
+      resolve(decodedToken);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+module.exports = { signToken, verifyToken };
