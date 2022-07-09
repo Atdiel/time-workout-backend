729364135089675625241df2c1c0d9aa98871cee
Atdiel <atdiel.perez9709@gmail.com>
2022-07-08 23:53:00 -0500

    fix: edited use handleHttpError function correctly :octocat:

--- BLL/api/middlewares/tokenMiddleware.js
+++ BLL/api/middlewares/tokenMiddleware.js
@@ -1,5 +1,5 @@
 const { handleHttpError } = require("../handlers/handleError");
-const { verifyToken } = require("../handlers/handleJwt");
+const { verifyToken } = require("../../helpers/jwtHelper");
 
 /**
  * middleware para verificar token jwt y mandar al controller un id
@@ -12,7 +12,7 @@ const authMiddleware = async (req, res, next) => {
   try {
     // si no existe la clave en el header
     if (!req.headers.authorization) {
-      return handleHttpError(res, "MISSING_TOKEN", 401);
+      return handleHttpError(res, [null, "Missing Token", 401]);
     }
     // creamos el token eliminando barer
     const token = req.headers.authorization.split(" ").pop();
@@ -20,7 +20,7 @@ const authMiddleware = async (req, res, next) => {
 
     // verificamos que tenga un id el token
     if (!dataToken._id) {
-      return handleHttpError(res, "ERROR_INVALID_TOKEN_ID");
+      return handleHttpError(res, [null, "Invalid Token", 401]);
     }
 
     // agregamos el id del usuario para usarlo luego en el controller
@@ -28,8 +28,7 @@ const authMiddleware = async (req, res, next) => {
 
     next();
   } catch (err) {
-    console.log(err);
-    handleHttpError(res, "ERROR_TOKEN_MIDDLEWARE", err);
+    handleHttpError(res, [err]);
   }
 };
 
