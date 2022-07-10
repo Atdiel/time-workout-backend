c25b8bbbd9c37c4e28ee83b7ec8d524000bc1e0e
Atdiel <atdiel.perez9709@gmail.com>
2022-07-09 23:02:32 -0500

    feat: add recordValidator add one and only one error message by field in the body-form

--- BLL/api/validators/recordValidator.js
+++ BLL/api/validators/recordValidator.js
@@ -1,36 +1,80 @@
 const { check, param } = require("express-validator");
 const handleResults = require("../handlers/handleValidator");
 
+/**
+ * > Validate record form.
+ */
 const recordValidator = [
   check("tittle")
     .exists()
+    .withMessage(
+      "Verify the API docs in https://time-workout.herokuapp.com/docs/v1/"
+    )
+    .bail()
     .notEmpty()
-    .withMessage("missing tittle")
-    .isLength({ max: 30 }),
+    .withMessage("tittle shouldn't be empty")
+    .bail()
+    .isLength({ max: 30 })
+    .withMessage("tittle too large"),
+
   check("description")
     .exists()
+    .withMessage(
+      "Verify the API docs in https://time-workout.herokuapp.com/docs/v1/"
+    )
+    .bail()
     .notEmpty()
-    .withMessage("missing description")
-    .isLength({ max: 120 }),
+    .withMessage("description shouldn't be empty")
+    .bail()
+    .isLength({ max: 120 })
+    .withMessage("description too large. Max length 120"),
+
+  check("recordTable")
+    .isObject()
+    .withMessage("recordTable should be an JSON object"),
+
   check("recordTable.records.*.timestamp")
     .exists()
-    .isISO8601()
-    .custom((value, { req }) => {
+    .withMessage(
+      "Verify the API docs in https://time-workout.herokuapp.com/docs/v1/"
+    )
+    .bail()
+    .custom((_, { req }) => {
       let obj = req.body.recordTable.records;
       if (obj.constructor.name == "Array") {
         return true;
       }
-      throw new Error("should been within an array");
-    }),
-  check("recordTable.records.*.amount").exists().isInt(),
+      throw new Error("records should be an array");
+    })
+    .bail()
+    .isISO8601()
+    .withMessage("timestamp invalid date"),
+
+  check("recordTable.records.*.amount")
+    .exists()
+    .withMessage(
+      "Verify the API docs in https://time-workout.herokuapp.com/docs/v1/"
+    )
+    .bail()
+    .isInt()
+    .withMessage("amount should been a number, try without quotes")
+    .bail()
+    .not()
+    .isIn([0])
+    .withMessage("amount shouldn't be a zero value"),
+
   handleResults,
 ];
 
+/**
+ * > Validate record ID in the URI.
+ */
 const recordIdValidator = [
   param("recordid")
     .exists()
     .withMessage("Should include recordid in the URI")
     .notEmpty(),
+
   handleResults,
 ];
 
