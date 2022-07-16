19cda39d5314eeac59c401b9ab56e5d214b063cf
Atdiel <atdiel.perez9709@gmail.com>
2022-07-15 23:50:15 -0500

    feat: add ssl settings

--- DAL/config/mysql.js
+++ DAL/config/mysql.js
@@ -6,6 +6,9 @@ const dbconf = {
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const connection = mysql.createConnection(dbconf);
